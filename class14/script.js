b805deb5731eb2e2ffc8b7c5c1c820f287e1d136
alihaiderbanoqabil <alihaider.banoqabil2.0@gmail.com>
2025-12-28 13:32:59 +0500

    done with class 2 of js

---
+++ class14/script.js
@@ -0,0 +1,263 @@
+console.log("Class 2 of Js");
+
+// // bad approach 
+// let student1 = "John Doe";
+// let student2 = "Jane Smith";
+// let student3 = "Alice Johnson";
+// let student4 = "Bob Brown";
+
+// console.log(student1);
+// console.log(student2);
+// console.log(student3);
+// console.log(student4);
+// better approach
+// let students1 = [
+//     "John Doe",
+//     "Jane Smith",
+//     "Alice Johnson",
+//     "Bob Brown"
+// ]
+
+// let students = ["John Doe", "Jane Smith", "Alice Johnson", "Bob Brown"];
+// students[2] = "Alice Cooper";
+// students[4] = "Charlie Davis";
+// delete students[4];
+// console.log("students", students);
+
+// let studentMarks = [85, 92, 78, 90];
+// let studentsIsAdult = [true, true, false, true];
+// let mixedArray = [10, "ali", false, null, undefined, {}, [1, 2, 3]];
+// let mixedArray = [, , , [1, 2, 3], ,];
+
+// console.log(mixedArray.length, "mixedArray.length");
+
+// console.log(students, "students");
+// console.log(students[1], "students");
+// console.log(studentMarks[3], "studentMarks");
+// console.log(studentMarks, "studentMarks");
+// console.log(studentsIsAdult, "studentsIsAdult");
+// console.log(mixedArray, "mixedArray");
+// // console.log(mixedArray[6], "mixedArray");
+// console.log(mixedArray[mixedArray.length - 1], "mixedArray");
+
+
+
+// let student1name = "John Doe";
+// let student1age = 20;
+// let student1isAdult = true;
+// let student1marks = [85, 90, 78];
+// let student1city = "New York";
+// let student1zip = "10001";
+// let student1street = "123 Main St"; 
+
+// let student2name = "John Doe";
+// let student2age = 20;
+// let student2isAdult = true;
+// let student2marks = [85, 90, 78];
+// let student2city = "New York";
+// let student2zip = "10001";
+// let student2street = "123 Main St";
+
+
+
+let student1 = {
+    name: "John Doe",
+    age: 20,
+    isAdult: true,
+    marks: [85, 90, 78],
+    address: {
+        street: "123 Main St",
+        city: "New York",
+        zip: "10001"
+    }
+};
+// console.log(student1["name"], "student1.name");
+// console.log(student1.name, "student1.name");
+// console.log(student1.address, "student1.address");
+// console.log(student1.address.city, "student1.address.city");
+// console.log(student1["address"]["city"], "student1.address.city");
+// console.log(student1.marks, "student1.marks");
+// console.log(student1.marks[2], "student1.marks");
+
+// let student2 = {
+//     name: "Jane Smith",
+//     age: 22,
+//     isAdult: true,
+//     marks: [88, 92, 81],
+//     address: {
+//         street: "456 Elm St",
+//         city: "Los Angeles",
+//         zip: "90001"
+//     }
+// };
+
+let students = [
+    {
+        name: "John Doe",
+        age: 20,
+        isAdult: true,
+        marks: [85, 90, 78],
+        address: {
+            street: "123 Main St",
+            city: "New York",
+            zip: "10001"
+        }
+    },
+    {
+        name: "Jane Smith",
+        age: 22,
+        isAdult: true,
+        marks: [88, 92, 81],
+        address: {
+            street: "456 Elm St",
+            city: "Los Angeles",
+            zip: "90001"
+        }
+    }
+];
+
+const product = {
+    "id": 808,
+    "name": "RGB Gaming Mouse Pad Large (800×300×4mm) LED Mousepad With Non-Slip Rubber Base Soft Pad",
+    "slug": "rgb-gaming-mouse-pad-large-800-300-4mm-led-mousepad-with-non-slip-rubber-base-soft-pad",
+    "sku": "SKU-20251215-23CUWA",
+    "barcode": "Gaming Mouse Pad Large",
+    "model": null,
+    "featured_image": "https://amanoon-bucket.s3.me-central-1.amazonaws.com/amanoon_pk_live/products/featured/sj9pPe5OxGIUhw6lr0kHOLtzz919LyMriroQcVpW.jpg",
+    "short_description": "<p><strong>Description</strong>:&nbsp;RGB Gaming Mouse Pad Large (800×300×4mm) Led Mousepad With Non-Slip Rubber Base Soft Pad –</p><p style=\"text-align: left;\"><strong>Detail</strong>:&nbsp;&nbsp;?RGB LED Backlit?Hcman RGB Mouse Pad Has A True RGB Backlit Effect,There Are 10 Lighting <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"http://Modes.You\">Modes.You</a> Can Turn Off The Light If You Want To. Also You Can Use Rainbow Mode Match Up With Hcman Backlit Mechanical Keyboard And Mouse</p><p>?10 Lighting Modes ?Super Glow Fiber – Chroma customizable lighting provides a distinctive appearance , Choose from 10 lighting modes : Rainbow breathing, Red , Purple ,Blue ,Cyan , Green , Yellow , Flowing Circling Motion , Colorful Circling Motion, turn off</p><p>?Optimum Size 31.5×11.8in?The gaming mouse pad will fit your desktop perfectly and provide perfect movement space . Applies to all types of keyboards and mice , providing plenty of room for professional gaming or office work</p><p>?Smooth Surface &amp; Anti-slip Rubber Base ?Designed with superfine fiber braided material , With a micro-textured, smooth surface , precise navigation. Glow fiber and soft cloth surface with Non-Slip rubber base ensures precise , accurate mouse control and consistent stability on your desk</p><p>?Easy to Use?This Soft backlit gaming mouse pad is powered by USB, plug and play, no driver needed. One-touch control designed to switch the light colors and turn off, very convenient to use</p><p style=\"text-align: left;\"><strong>Weight</strong>:&nbsp;1.00 KGs</p>",
+    "description": "<p></p>",
+    "is_active": 1,
+    "fulfilled": 1,
+    "is_featured": 1,
+    "product_type": "simple",
+    "condition": "new",
+    "length": 0,
+    "width": 0,
+    "height": 0,
+    "weight": 0,
+    "delivery_fees": null,
+    "tax_id": 1,
+    "parent_id": null,
+    "brand_id": 64,
+    "created_by_id": 4,
+    "updated_by_id": 4,
+    "meta_title": "Buy Large RGB Gaming Mouse Pad 800×300 LED Non-Slip Price in Pakistan",
+    "meta_description": "Buy Large RGB Gaming Mouse Pad 800×300 LED with Non-Slip Base. Enhance your gaming setup with vibrant lights. Price in Pakistan.",
+    "meta_keywords": "Large RGB Gaming Mouse Pad 800×300 LED Non-Slip Price in Pakistan, Large RGB Gaming Mouse Pad 800×300 LED Non-Slip Price in Karachi, Large RGB Gaming Mouse Pad 800×300 LED Non-Slip Price in Lahore, Large RGB Gaming Mouse Pad 800×300 LED Non-Slip Price in Islamabad, Large RGB Gaming Mouse Pad 800×300 LED Non-Slip Price in Peshawar",
+    "created_at": "2025-12-15T06:36:28.000000Z",
+    "updated_at": "2025-12-15T06:36:28.000000Z",
+    "price": 0,
+    "sale_price": null,
+    "final_price": 0,
+    "discount_percentage": 0,
+    "in_stock": false,
+    "seller": "atReBoot",
+    "seller_slug": "atreboot",
+    "brand": {
+        "id": 64,
+        "name": "No Brand",
+        "slug": "no-brand"
+    },
+    "categories": [
+        {
+            "id": 37,
+            "name": "Input Devices",
+            "slug": "input-device",
+            "commission_rate": 0,
+            "pivot": {
+                "product_id": 808,
+                "category_id": 37
+            }
+        },
+        {
+            "id": 39,
+            "name": "Mouse Pads",
+            "slug": "mouse-pads",
+            "commission_rate": 0,
+            "pivot": {
+                "product_id": 808,
+                "category_id": 39
+            }
+        }
+    ],
+    "inventories": []
+}
+// product.categories[0].pivot.category_id = 100;
+// delete product.categories[0].pivot.category_id;
+// delete product.categories[0].slug;
+// console.log(product, "product");
+
+// console.log(students, "students");
+
+
+// let address = {
+//     street: "123 Main St",
+//     city: "New York",
+//     zip: "10001"
+// };
+
+
+// // let name = student1.name;
+// let name1 = "Ali Ahmed";
+// console.log("name1: ", name1);
+// let name2 = name1;
+// name2 = "Sami Khan";
+// console.log("name2: ", name2);
+
+
+// let student2 = student1;
+// let student2 = { ...student1, address: { ...student1.address } }; // shallow copy
+// console.log(student1, "student1");
+// student2.name = "Jane Smith";
+// student2.address.city = "Karachi";
+// console.log(student2, "student2");
+
+const productString = JSON.stringify(product);
+const productObject = JSON.parse(productString);
+const student2 = JSON.parse(JSON.stringify(student1));
+// console.log(product, "product");
+// console.log(productString, "productString");
+// console.log(productObject, "productObject");
+// let student2 = structuredClone(student1); // deep copy
+console.log(student1, "student1");
+student2.name = "Jane Smith";
+student2.address.city = "Karachi";
+console.log(student2, "student2");
+
+let name = "Ali Ahmed";
+let age = 25;
+let isAdult = true;
+
+// console.log(typeof student1);
+// console.log(typeof students);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isAdult);
+// console.log(typeof null);
+
+let num1 = "10";
+let num2 = "20";
+let number1 = Number(num1);
+let number2 = Number(num2);
+console.log(number1 + number2);
+console.log(num1 + num2);
+console.log(typeof num1);
+console.log(typeof number1);
+
+console.log(Number("abc"));
+
+console.log(typeof String(100));
+
+console.log(Boolean(""));
+console.log(Boolean([]));
+console.log(Boolean("alakklajlk"));
+console.log(Boolean(0));
+console.log(Boolean({}));
+
+
+
+
+// console.log("10" + "20");
+
+
+
